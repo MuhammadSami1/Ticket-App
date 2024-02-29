4c40ff90714ab8fedcfce2d2fb38abcbb4a252bc
muhammadsami <muhammadsami1242@gmail.com>
2024-02-29 21:26:14 +0500

    project complete

--- app/(components)/ProgressDisplay.jsx
+++ app/(components)/ProgressDisplay.jsx
@@ -1,9 +1,9 @@
-const ProgressDisplay = () => {
+const ProgressDisplay = ({ progress }) => {
   return (
     <div className="bg-gray-200 w-full rounded-full h-2.5 ">
       <div
         className="bg-blue-600 h-2.5 rounded-full"
-        style={{ width: "75%" }}
+        style={{ width: `${progress}%` }}
       ></div>
     </div>
   );
